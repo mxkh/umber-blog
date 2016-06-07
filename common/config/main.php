6ff0aded8da3caa9fb2fad43563c7c6fb51adc36
Max <maxkhartanovich@gmail.com>
2016-06-07 14:57:58 +0300

    hotfix 0.0.2: update config

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=umberblog',
+            'username' => 'dbuser',
+            'password' => '123',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
